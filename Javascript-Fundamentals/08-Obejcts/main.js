a356faed480b4c870f97a68f3e847ab4681003ce
Nathan <108722893+xNapha@users.noreply.github.com>
2023-01-03 15:38:42 +1100

    Finished Objects and Classes lessons

--- Javascript-Fundamentals/08-Obejcts/main.js
+++ Javascript-Fundamentals/08-Obejcts/main.js
@@ -21,6 +21,8 @@ console.log(exampleObject.method());
 console.log(exampleObject["types"])
 console.log(exampleObject["key"])
 */
+
+
 const studentsCon = document.querySelector(".studentsCon");
 
 const person = {
@@ -56,7 +58,9 @@ const person = {
     return HTML;
   }
 };
-studentsCon.innerHTML = person.getHTML();
+//studentsCon.innerHTML = person.getHTML();
+
+
 /*
 studentsCon.innerHTML = `
 <article class="studentCard">
@@ -83,3 +87,115 @@ studentsCon.innerHTML = `
 //console.log(person.getFullName());
 
 //https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png
+
+
+// CLASS SYNTAX
+
+// PascalCase -> naming convention for classes
+// class ExampleClass{
+//   constructor(firstName, lastName, age){
+//     this.firstName = firstName,this.lastName = lastName,this.age = age
+//   };
+//   greet(){
+//     let greeting = `Hello ${this.firstName} ${this.lastName}`
+//     return greeting;
+//   }
+// };
+// CLASS INHERITANCE SYNTAX
+
+// class ExtendedClass extends ExampleClass{
+//   constructor(firstName, lastName, age, subject){
+//     super(firstName, lastName, age),this.subject = subject;
+//   };
+//   greet(){
+//     let teacherGreeting = `Hi I am Mr.${this.lastName} and this is class is for ${this.subject}.`
+//     return teacherGreeting
+//   }
+// }
+// console.log(new ExtendedClass("Charlie", "Richardson", 30, "JavaSript").greet())
+
+
+// const nathan = new ExampleClass("Nathan", "Cai",25);
+// const bob = new ExampleClass("Bob", "Bill", 30);
+// console.log(nathan.greet())
+// console.log(bob.greet())
+
+class Person{
+  constructor(firstName, lastName, quote, skills, imgURL){
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.quote = quote, 
+    this.skills = skills,
+    this.imgURL = imgURL
+  };
+  getSkills(){
+    const skillsString = this.skills.map(skill=>`<li>${skill}</li>`);
+    return skillsString.join('');
+  };
+  getFullName(){
+    let fullName = `${this.firstName} ${this.lastName}`
+    return fullName;
+  };
+  getHTML(){
+    const HTML = `<article class="studentCard">
+    <img src=${this.imgURL}>
+    <h2>${this.getFullName()}</h2>
+    <blockquote>${this.quote}</blockquote>
+    <h3>Skills</h3>
+    <ul>${this.getSkills()}</ul>
+    </article>`;
+    return HTML;
+  };
+};
+
+class VIP extends Person{
+  constructor(firstName, lastName, quote, skills, imgURL, contactLink){
+    super(firstName, lastName, quote, skills, imgURL),
+    this.contactLink = contactLink
+  };
+  getHTML(){
+    const HTML = `<article class="studentCard">
+    <img src="${this.imgURL}">
+    <h2>${this.getFullName()}</h2>
+    <blockquote>${this.quote}</blockquote>
+    <h3>Skills</h3>
+    <ul>${this.getSkills()}</ul>
+    <a href=${this.contactLink} target="blank">Contact me</a>
+    </article>`;
+    return HTML;
+  };
+}
+
+const jim = new Person(
+  "Jim", 
+  "Harper", 
+  "Dammit Jim",
+  ["Salesman", "Comedic", "Stubborn"], 
+  "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png"
+);
+const pam = new Person(
+  "Pam", 
+  "Harper", 
+  "Dammit Michael",
+  ["Salesman", "Comedic", "Stubborn"], 
+  "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png"
+);
+const michael = new VIP(
+  "Michael",
+  "Scott",
+  "Snip, Snap, Snip, Snap",
+  ["Nothing", "Nothing","Nothing"],
+  "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png",
+  "https://www.google.com"
+)
+// console.log(jim.getSkills())
+// console.log(jim.getFullName())
+// console.log(jim.getHTML())
+
+
+studentsCon.innerHTML += jim.getHTML();
+studentsCon.innerHTML += pam.getHTML();
+studentsCon.innerHTML += michael.getHTML();
+
+
+
