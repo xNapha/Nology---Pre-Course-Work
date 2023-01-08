f9c6a5ac9161a1934436c8983b69545fe8334351
Nathan <108722893+xNapha@users.noreply.github.com>
2023-01-08 16:50:25 +1100

    Update
    Functionality overhaul
    Wanting to satisfy multiple different quality of life features was causing me to create messy code.
    This was cause because i didnt have a clear vision of what i wanted.
    Now i have a clear vision of what i want so now it should be easier to finish this project

--- Javascript-Fundamentals/Project-01-Calculator/calculator.js
+++ Javascript-Fundamentals/Project-01-Calculator/calculator.js
@@ -1,71 +1,51 @@
 // when buttons are pressed show input on the display
 const mainDisplay = document.querySelector("#mainDisplay");
-let previousValue = "";
+let previousValue = "0";
 let currentValue = "";
-let previousOperator = "";
+let currentOperator = "";
 let equalsPressed = false;
 const buttons = document.querySelectorAll(".buttons");
 buttons.forEach(button => button.addEventListener("click", (event)=>{
   const btnVal = event.target.value;
+  if(currentOperator != ""){
+    
+  }
   switch(btnVal){
     case "(":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
       break;
     case ")":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
       break;
     case "/100":
-      if(currentValue == "" || currentValue == "0"){
-        alert("Enter a valid number before proceeding")
-      } else{
-        mainDisplay.innerHTML = `${currentValue/100}%`
-        previousValue = currentValue/100;
-        previousOperator = "%";
-      }
       break;
     case "Clear":
       break;
     case "/":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
+      currentOperator = "/";
       break;
     case "*":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
+      currentOperator = "*";
       break;
     case "-":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
+      currentOperator = "-";
       break;
     case "+":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
-      previousOperator = "+";
+      currentOperator = "+";
+      previousValue = currentValue;
       break;
     case "=":
-      if(previousOperator == "+"){
-        previousValue = parseInt(currentValue) + parseInt(previousValue);
-        mainDisplay.innerHTML = ` ${previousValue}`;
-        equalsPressed = true;
-      }
+      equalsPressed = true;
       break;
     case ".":
-      mainDisplay.innerHTML += ` ${event.target.value}`;
       break;
     default:
-      if(equalsPressed){
-        previousValue = "";
-        currentValue = event.target.value;
-        mainDisplay.innerHTML = `${event.target.value}`
-        equalsPressed = false;
-      } else if(previousOperator == "+"){
-        previousValue = currentValue;
-        mainDisplay.innerHTML += ` ${currentValue}`
-      }else if(previousOperator == "%"){
-        previousValue = "";
-        mainDisplay.innerHTML = ` ${event.target.value}`;
-      }else{
-        currentValue += event.target.value;
-        mainDisplay.innerHTML += `${event.target.value}`
-      }
+      currentValue += event.target.value;
+      mainDisplay.innerHTML += `${event.target.value}`;
   };
 }));
+
+function addition(previous, current){
+  mainDisplay.innerHTML = ` ${parseInt(previous) + parseInt(current)}`;
+}
 
 
 
