4b82c11951dc240019439dfb5005eae2ea4ed626
Nathan <108722893+xNapha@users.noreply.github.com>
2023-01-11 11:24:01 +1100

    Update
    Added the functionality to add the piece to the bottom of that column respective to that column

--- Javascript-Fundamentals/Project-02-Game/imgs/connect4.js
+++ Javascript-Fundamentals/Project-02-Game/imgs/connect4.js
@@ -7,30 +7,55 @@ gameBoardCon.setAttribute("id", "gameBoardCon");
 body.append(gameBoardCon);
 let gameBoardArr = [];
 
+const redPiece = "red";
+const yellowPiece = "yellow";
+let isRed = true;
+
 //Populate Array with arrays of objects
 function populateBoard(){
-  for(let x = 0; x < 7; x++){
+  for(let y = 0; y < 6; y++){
     const row = [];
-    for(let y = 0; y< 6; y++){
+    for(let x = 0; x< 7; x++){
       const div = document.createElement("div");
       div.classList.add("empty");
-      const space ={
-        div,
-        x,
-        y,
-      };
+      div.addEventListener("click",()=>{
+        addMarker(x)
+        swapPlayers();
+      })
+      const space = {div,x,y,taken:false,};
       row.push(space);
     };
     gameBoardArr.push(row);
   };
   gameBoardArr.forEach(row =>{
     row.forEach(space=>{
-      gameBoardCon.append(space.div)
+      gameBoardCon.append(space.div);
     });
   });
   return gameBoardArr;
 }
 
+// Swap colours after each click
+function swapPlayers(){
+  isRed = !isRed;
+}
+
+// Move colours down to the very bottom of the column once a column once the column has been pressed
+// when column has been clicked check all y values until a has not been taken
+function addMarker(x){
+  for(let posY = 5; posY > -1; posY--){
+    if(!gameBoardArr[posY][x].taken){
+      if(isRed){
+        gameBoardArr[posY][x].div.style.backgroundColor = redPiece;
+        gameBoardArr[posY][x].taken = true;
+        break;
+      }else{
+        gameBoardArr[posY][x].div.style.backgroundColor = yellowPiece;
+        gameBoardArr[posY][x].taken = true;
+        break;
+      };
+    };
+  };
+}
 //run function to visualise gameboard
 populateBoard();
-console.log(gameBoardArr)
