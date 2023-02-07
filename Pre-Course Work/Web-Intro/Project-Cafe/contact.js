c7126efd9aad3e3c66fafa4fa98e709e9b01ec93
xNapha <nathancai1997@gmail.com>
2023-02-07 11:34:50 +1100

    File Update

--- Pre-Course Work/Web-Intro/Project-Cafe/contact.js
+++ Pre-Course Work/Web-Intro/Project-Cafe/contact.js
@@ -1,52 +1,53 @@
-const form = document.querySelector("form")
-form.onsubmit = function(event){
+const form = document.querySelector("form");
+form.onsubmit = function (event) {
   event.preventDefault();
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const comment = document.getElementById("comment").value;
-  
-  const newMsg = message(name,email,comment);
+
+  const newMsg = message(name, email, comment);
 
   let currentMsgs = [];
-  if (window.sessionStorage.getItem("messages")){
+  if (window.sessionStorage.getItem("messages")) {
     currentMsgs = JSON.parse(window.sessionStorage.getItem("messages"));
-  };
+  }
   currentMsgs.push(newMsg);
 
   window.sessionStorage.setItem("messages", JSON.stringify(currentMsgs));
   displayMsgs();
-}
+};
 
-const message = (n,e,c) => {
+const message = (n, e, c) => {
   const name = n;
   const email = e;
   const comment = c;
-  return{name, email, comment}
-}
+  return { name, email, comment };
+};
 
-function displayMsgs(){
+function displayMsgs() {
   let currentMsgs = [];
-  if (window.sessionStorage.getItem("messages")){
+  if (window.sessionStorage.getItem("messages")) {
     currentMsgs = JSON.parse(window.sessionStorage.getItem("messages"));
-  };
+  }
 
   let listMsgs = [];
-  for(let i = 0; i < currentMsgs.length; i++){
+  for (let i = 0; i < currentMsgs.length; i++) {
     let listMsg = "";
     let currentMsg = currentMsgs[i];
-    listMsg += `<dt> ${currentMsg.name} | ${currentMsg.email}</dt>`
-    listMsg += `<dd> ${currentMsg.comment} </dd>`
-    listMsg += `<br/>`
+    listMsg += `<dt> ${currentMsg.name} | ${currentMsg.email}</dt>`;
+    listMsg += `<dd> ${currentMsg.comment} </dd>`;
+    listMsg += `<br/>`;
     listMsgs.push(listMsg);
   }
-  const displayList = document.getElementById("currentMsgs")
+  const displayList = document.getElementById("currentMsgs");
   displayList.innerHTML = listMsgs.join("");
 }
 displayMsgs();
 
-function deleteMsgs(){
-  if (window.sessionStorage.getItem("messages")){
+function deleteMsgs() {
+  if (window.sessionStorage.getItem("messages")) {
     window.sessionStorage.removeItem("messages");
-  };
+  }
   displayMsgs();
 }
+document.body.querySelector("#clear").setAttribute("onclick", "deleteMsgs()");
